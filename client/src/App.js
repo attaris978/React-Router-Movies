0fbfc8be7a682ab6f78a5c49ec94c55660a54558
unknown <ryancare@protonmail.com>
2022-03-28 16:15:24 -0500

    Added route for exact path / and passed gotten state

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Route} from 'react-router-dom';
+import MovieList from "./Movies/MovieList";
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -12,6 +13,7 @@ export default function App () {
       axios
         .get('http://localhost:5001/api/movies') // Study this endpoint with Postman
         .then(response => {
+          setMovieList(response.data);
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -25,12 +27,14 @@ export default function App () {
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
-
+  if (!movieList) return (<h1>Awaiting the requested list ...</h1>);
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/">
+        <MovieList movies={movieList} />
+      </Route>
     </div>
   );
 }
